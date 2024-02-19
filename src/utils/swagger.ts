f8414e79309cf65ca0f52fc19522ceab552df9d1
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-02-19 19:41:46 +0400

    Add schemas for the swagger documentation

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -3,20 +3,19 @@ import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import { version } from "../../package.json";
 import logger from "./logger";
-import YAML from "yamljs";
 
 const options: swaggerJsdoc.Options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "REST API TS Docs",
+      title: "REST API Docs",
       version,
     },
     components: {
-      securitySchemas: {
+      securitySchemes: {
         bearerAuth: {
           type: "http",
-          schema: "bearer",
+          scheme: "bearer",
           bearerFormat: "JWT",
         },
       },
@@ -39,7 +38,7 @@ const swaggerDocs = (app: Express, port: number) => {
   app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
   //    Docs to JSON format
-  app.get("docs.json", (req: Request, res: Response) => {
+  app.get("/docs.json", (req: Request, res: Response) => {
     res.setHeader("Content-Type", "application/json");
     res.send(swaggerSpec);
   });
